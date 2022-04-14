0e83d9e785b4a9c39684543a93e22a2b1f8a34df
Song Jiyeon <jiyeon.song@riiid.co>
2022-04-14 17:59:09 +0900

    📦️ Update package name to BestSafariView

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "BetterSafariView",
+    name: "BestSafariView",
     platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS("6.2")],
     products: [
         .library(name: "BetterSafariView", targets: ["BetterSafariView"])
